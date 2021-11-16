8f158218638d4bbecc16816e47c9052c07934d16
Aslan <korpeevaslan@gmail.com>
2021-11-17 00:38:52 +0500

    deploy

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use("/api", TodoRoutes);
 app.use("/api", FolderRoutes);
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.htmls"));
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 app.use(errorMiddleware);
 const PORT = process.env.PORT || 5000;
