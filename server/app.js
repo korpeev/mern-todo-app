ad46c960f212e628e7a40da85fcca88d0e2f8589
Aslan <korpeevaslan@gmail.com>
2021-11-16 06:49:30 +0500

    fixed: imported path

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const mongosoe = require("mongoose");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
